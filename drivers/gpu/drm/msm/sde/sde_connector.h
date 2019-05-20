588e5dfe6376fe69ee58fe7e57b7c3aebf280443
Raviteja Tamatam <travitej@codeaurora.org>
2019-05-20 14:47:23 +0530

    drm/msm/sde: fix race condition in connector state mode info
    There is a race condition where connector state mode info is updated
    in sde_encoder_virt_mode_set in crtc_commit thread and mode info
    getting copied to new connector state in connector_atomic_duplicate_state
    in hwbinder thread. This is causing new connector state to be in
    invalid state leading to wrong dsc config and further leading
    to ping pong timeouts.Connector state mode info update is protected
    by mutex lock to avoid this.
    
    Change-Id: I2a13a85ff3fd2198c31fb9d929b0c127dcc520ed
    Signed-off-by: Raviteja Tamatam <travitej@codeaurora.org>
    Signed-off-by: Naseer Ahmed <naseer@quicinc.com>
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- drivers/gpu/drm/msm/sde/sde_connector.h
+++ drivers/gpu/drm/msm/sde/sde_connector.h
@@ -341,6 +341,7 @@ struct sde_connector_evt {
  * @allow_bl_update: Flag to indicate if BL update is allowed currently or not
  * @last_cmd_tx_sts: status of the last command transfer
  * @hdr_capable: external hdr support present
+ * @mode_info_lock: lock to protect mode info
  */
 struct sde_connector {
 	struct drm_connector base;
@@ -389,6 +390,7 @@ struct sde_connector {
 
 	bool last_cmd_tx_sts;
 	bool hdr_capable;
+	struct mutex mode_info_lock;
 	bool panel_dead_skip;
 };
 
